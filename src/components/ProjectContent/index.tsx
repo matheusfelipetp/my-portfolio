d7a92dddb0f92613f2e16fe37480782bce747de3
matheusfelipetp <matheusfelipetp@outlook.com>
2023-02-25 17:12:24 -0300

    feat: add cards in projects component

--- src/components/ProjectContent/index.tsx
+++ src/components/ProjectContent/index.tsx
@@ -1,8 +1,25 @@
 import { MobileContext } from '@/context/MobileContext';
-import { useContext } from 'react';
+import useMedia from '@/hooks/useMedia';
+import { useContext, useEffect, useState } from 'react';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 export const ProjectContent = () => {
+  const [isBtnVisible, setIsBtnVisible] = useState<boolean>(false);
+  const [isCardVisible, setIsCardVisible] = useState<boolean>(false);
+  const [pagination, setPagination] = useState<number>(1);
+
+  const mobile = useMedia('(max-width:60rem)');
   const { mobileMenu } = useContext(MobileContext);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsBtnVisible(true);
+    }, 600);
+
+    setTimeout(() => {
+      setIsCardVisible(true);
+    }, 900);
+  }, []);
 
   return (
     <section className={`projects ${mobileMenu && 'opacity'}`}>
@@ -14,7 +31,31 @@ export const ProjectContent = () => {
           Abaixo, estão listados alguns dos principais projetos que desenvolvi.
           Caso deseje visualizar todos, acesse o meu <span>GitHub</span>.
         </p>
+
+        {isBtnVisible && (
+          <a
+            className="btn-github animate__animated animate__fadeInUp"
+            href="https://github.com/matheusfelipetp"
+            target="_blank"
+          >
+            Ver todos os projetos
+            <BsArrowRightShort className="arrow" />
+          </a>
+        )}
       </div>
+
+      {isCardVisible && (
+        <div className="projects-cards animate__animated animate__fadeIn">
+          <div className="card">1</div>
+          <div className="card">2</div>
+          <div className="card">3</div>
+          <div className="card">4</div>
+          <div className="card">5</div>
+          <div className="card">6</div>
+          <div className="card">7</div>
+          <div className="card">8</div>
+        </div>
+      )}
     </section>
   );
 };
